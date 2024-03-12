9281c4e8a29f59a403d3303a1c4958318bbb5f90
varun7778 <34128397+varun7778@users.noreply.github.com>
2024-03-12 19:26:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def input_image_setup(uploaded_file):
 
 st.set_page_config(page_title="Gemini Health App")
 
-st.header("Gemini Health App")
+st.header("Gemini Calorie Counter App")
 input=st.text_input("Input Prompt: ",key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 image=""   
@@ -46,7 +46,7 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
 
-submit=st.button("Tell me the total calories")
+submit=st.button("Total calories")
 
 input_prompt="""
 You are an expert in nutritionist where you need to see the food items from the image
@@ -64,6 +64,5 @@ is below format
 if submit:
     image_data=input_image_setup(uploaded_file)
     response=get_gemini_repsonse(input_prompt,image_data,input)
-    st.subheader("The Response is")
+    st.subheader("Total Calories in the given food are:")
     st.write(response)
-
